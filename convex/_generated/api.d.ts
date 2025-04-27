aae49d6d6215a4ea8c72cd52a35ceea2e95692d6
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-04-27 12:37:11 +0200

    Make prettier ignore convex dirs

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -12,13 +12,13 @@ import type {
   ApiFromModules,
   FilterApi,
   FunctionReference,
-} from 'convex/server';
-import type * as auth from '../auth.js';
-import type * as http from '../http.js';
-import type * as messages from '../messages.js';
-import type * as otp_ResendOTP from '../otp/ResendOTP.js';
-import type * as otp_VerificationCodeEmail from '../otp/VerificationCodeEmail.js';
-import type * as users from '../users.js';
+} from "convex/server";
+import type * as auth from "../auth.js";
+import type * as http from "../http.js";
+import type * as messages from "../messages.js";
+import type * as otp_ResendOTP from "../otp/ResendOTP.js";
+import type * as otp_VerificationCodeEmail from "../otp/VerificationCodeEmail.js";
+import type * as users from "../users.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -32,15 +32,15 @@ declare const fullApi: ApiFromModules<{
   auth: typeof auth;
   http: typeof http;
   messages: typeof messages;
-  'otp/ResendOTP': typeof otp_ResendOTP;
-  'otp/VerificationCodeEmail': typeof otp_VerificationCodeEmail;
+  "otp/ResendOTP": typeof otp_ResendOTP;
+  "otp/VerificationCodeEmail": typeof otp_VerificationCodeEmail;
   users: typeof users;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
-  FunctionReference<any, 'public'>
+  FunctionReference<any, "public">
 >;
 export declare const internal: FilterApi<
   typeof fullApi,
-  FunctionReference<any, 'internal'>
+  FunctionReference<any, "internal">
 >;
