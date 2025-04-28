6bfcdd0895930d63f0032df63613fd63d5c396a6
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-04-28 17:54:50 +0200

    fix: rename table

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,8 +1,8 @@
 import { integer, pgTable, varchar } from 'drizzle-orm/pg-core';
 
-export const usersTable = pgTable('users', {
+export const nodesTable = pgTable('nodes', {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
+  location: varchar({ length: 255 }),
+  description: varchar({ length: 255 }),
 });
