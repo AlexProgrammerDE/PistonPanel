08a2778fccafbad6932327a3f5039ec732efb966
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-04-30 09:55:40 +0200

    refactor: drop more uses of legacy codebase

--- src/routes/_dashboard/instance/$instance/meta.tsx
+++ src/routes/_dashboard/instance/$instance/meta.tsx
@@ -1,26 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { use } from 'react';
 import InstancePageLayout from '@/components/nav/instance-page-layout';
 import { useTranslation } from 'react-i18next';
-import {
-  useMutation,
-  useQueryClient,
-  useSuspenseQuery,
-} from '@tanstack/react-query';
-import { TransportContext } from '@/components/providers/transport-context';
-import {
-  formatIconName,
-  hasInstancePermission,
-  setInstanceFriendlyName,
-  setInstanceIcon,
-} from '@/lib/utils';
-import { GenericEntryComponent } from '@/components/settings-page';
-import { getAllIconTags } from '@/components/dynamic-icon';
-import {
-  InstancePermission,
-  StringSetting_InputType,
-} from '@/generated/pistonpanel/common';
-import { JsonValue } from '@protobuf-ts/runtime/build/types/json-typings';
 
 export const Route = createFileRoute('/_dashboard/instance/$instance/meta')({
   component: MetaSettings,
@@ -45,104 +25,10 @@ function MetaSettings() {
 }
 
 function Content() {
-  const { instanceInfoQueryOptions, instanceListQueryOptions } =
-    Route.useRouteContext();
-  const queryClient = useQueryClient();
-  const transport = use(TransportContext);
-  const { data: instanceInfo } = useSuspenseQuery(instanceInfoQueryOptions);
-  const setFriendlyNameMutation = useMutation({
-    mutationFn: async (value: JsonValue) => {
-      await setInstanceFriendlyName(
-        value as string,
-        instanceInfo,
-        transport,
-        queryClient,
-        instanceInfoQueryOptions.queryKey,
-        instanceListQueryOptions.queryKey,
-      );
-    },
-    onSettled: async () => {
-      await Promise.all([
-        queryClient.invalidateQueries({
-          queryKey: instanceInfoQueryOptions.queryKey,
-        }),
-        queryClient.invalidateQueries({
-          queryKey: instanceListQueryOptions.queryKey,
-        }),
-      ]);
-    },
-  });
-  const setIconMutation = useMutation({
-    mutationFn: async (value: JsonValue) => {
-      await setInstanceIcon(
-        value as string,
-        instanceInfo,
-        transport,
-        queryClient,
-        instanceInfoQueryOptions.queryKey,
-        instanceListQueryOptions.queryKey,
-      );
-    },
-    onSettled: async () => {
-      await Promise.all([
-        queryClient.invalidateQueries({
-          queryKey: instanceInfoQueryOptions.queryKey,
-        }),
-        queryClient.invalidateQueries({
-          queryKey: instanceListQueryOptions.queryKey,
-        }),
-      ]);
-    },
-  });
-
   return (
     <div className="flex h-full w-full max-w-4xl grow flex-col gap-4">
       <div className="flex flex-col gap-2">
-        <GenericEntryComponent
-          entry={{
-            oneofKind: 'string',
-            string: {
-              uiName: 'Friendly Name',
-              description:
-                'The name of the instance that will be displayed in the UI.',
-              def: '',
-              inputType: StringSetting_InputType.TEXT,
-              placeholder: 'My Instance',
-              minLength: 3,
-              maxLength: 32,
-              pattern: '[a-zA-Z0-9 ]+',
-              disabled: !hasInstancePermission(
-                instanceInfo,
-                InstancePermission.UPDATE_INSTANCE_META,
-              ),
-            },
-          }}
-          value={instanceInfo.friendlyName}
-          changeCallback={setFriendlyNameMutation.mutate}
-        />
-        <GenericEntryComponent
-          entry={{
-            oneofKind: 'combo',
-            combo: {
-              uiName: 'Icon',
-              description:
-                'The icon of the instance that will be displayed in the UI.',
-              options: getAllIconTags().map((iconName) => ({
-                id: iconName[0],
-                displayName: formatIconName(iconName[0]),
-                iconId: iconName[0],
-                keywords: iconName[1],
-              })),
-              def: '',
-              disabled: !hasInstancePermission(
-                instanceInfo,
-                InstancePermission.UPDATE_INSTANCE_META,
-              ),
-            },
-          }}
-          value={instanceInfo.icon}
-          changeCallback={setIconMutation.mutate}
-        />
+        <p>TODO: Add a content.</p>
       </div>
     </div>
   );
--- src/routes/_dashboard/user/settings.tsx
+++ src/routes/_dashboard/user/settings.tsx
@@ -1,12 +1,12 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { useTranslation } from 'react-i18next';
-import { ComponentTitle } from '@/components/settings-page';
 import UserPageLayout from '@/components/nav/user-page-layout';
 import { ExternalLink } from '@/components/external-link';
 import { UserAvatar } from '@/components/user-avatar';
 import { Card } from '@/components/ui/card';
 import { SettingsCards } from '@daveyplate/better-auth-ui';
 import { useSuspenseQuery } from '@tanstack/react-query';
+import { TextInfoButton } from '@/components/info-buttons';
 
 export const Route = createFileRoute('/_dashboard/user/settings')({
   component: UserSettings,
@@ -30,22 +30,24 @@ function Content() {
     <div className="flex h-full w-full max-w-4xl grow flex-col gap-4">
       <div className="flex flex-col gap-2">
         <div className="flex max-w-xl flex-col gap-1">
-          <ComponentTitle
-            title="Avatar"
-            description={
-              <>
-                Your user avatar is based on your account email address. You can
-                change the avatar for your email address at{' '}
-                <ExternalLink
-                  className="font-semibold underline-offset-4 hover:underline"
-                  href="https://gravatar.com"
-                >
-                  Gravatar
-                </ExternalLink>
-                .
-              </>
-            }
-          />
+          <div className="flex w-fit flex-row items-center gap-2">
+            <p>Avatar</p>
+            <TextInfoButton
+              value={
+                <>
+                  Your user avatar is based on your account email address. You
+                  can change the avatar for your email address at{' '}
+                  <ExternalLink
+                    className="font-semibold underline-offset-4 hover:underline"
+                    href="https://gravatar.com"
+                  >
+                    Gravatar
+                  </ExternalLink>
+                  .
+                </>
+              }
+            />
+          </div>
           <Card className="flex w-fit items-center gap-2 p-3 text-left text-base">
             <UserAvatar
               username={session.user.username ?? ''}
